c6b403508c9e26a797eea42bc109bbdca7925777
zaidanshori <m.zaidanshori04@gmail.com>
2025-10-30 07:07:27 +0700

    initial project

---
+++ src/main/java/com/zaid/CoinMarketApi/CoinMarketApiApplication.java
@@ -0,0 +1,13 @@
+package com.zaid.CoinMarketApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinMarketApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinMarketApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaid/CoinMarketApi/CoinMarketApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaid.CoinMarketApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoinMarketApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
